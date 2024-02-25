5a32e82a9450b2083f0a8873512be16699d60790
labidimahdi <labidimahdi99@gmail.com>
2024-02-25 03:02:45 +0100

    feat/portfolio1

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div
-    style={{backgroundImage: "url(/mountains.jpg)"}}
+    style={{backgroundImage: "url(/bg-2.jpg)"}}
      className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
         <div className="flex flex-col items-center gap-4">
           <h1 className="font-semibold text-white text-[50px]">
