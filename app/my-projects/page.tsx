3d9db33d9bf95426ccb797a563a5181b0e65cb98
labidimahdi <labidimahdi99@gmail.com>
2024-02-25 02:49:29 +0100

    feat/portfolio

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,26 +1,36 @@
-// "use client"
+"use client"
 
-// import ProjectCard from '@/components/ProjectCard'
-// import { Projects } from '@/constants'
-// import React from 'react'
+import ProjectCard from '@/components/ProjectCard'
+import { Projects } from '@/constants'
+import React from 'react'
 
-// const Page = () => {
-//   return (
-//     <div
-//     style={{backgroundImage: "url(/mountains.jpg)"}}
-//      className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
-//       <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
-//         {Projects.map((project, index) => (
-//           <ProjectCard
-//               key={index}
-//               title={project.title}
-//               text={project.text}
-//               image={project.src}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
+const Page = () => {
+  return (
+    <div
+    style={{backgroundImage: "url(/mountains.jpg)"}}
+     className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="font-semibold text-white text-[50px]">
+            {" still "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+              {"working "}
+              {" "}
+            </span>
+            on that
+          </h1>
+          </div>
+      {/* <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
+        {Projects.map((project, index) => (
+          <ProjectCard
+              key={index}
+              title={project.title}
+              text={project.text}
+              image={project.src}
+          />
+        ))}
+      </div> */}
+    </div>
+  )
+}
 
-// export default Page
+export default Page
