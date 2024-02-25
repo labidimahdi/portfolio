3bfa2ccb27b2fe05e0b3c3937edbfd0d5427c8f9
labidimahdi <labidimahdi99@gmail.com>
2024-02-25 03:05:16 +0100

    feat/portfolio2

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,7 +1,4 @@
 "use client"
-
-import ProjectCard from '@/components/ProjectCard'
-import { Projects } from '@/constants'
 import React from 'react'
 
 const Page = () => {
@@ -19,16 +16,7 @@ const Page = () => {
             on that
           </h1>
           </div>
-      {/* <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
-        {Projects.map((project, index) => (
-          <ProjectCard
-              key={index}
-              title={project.title}
-              text={project.text}
-              image={project.src}
-          />
-        ))}
-      </div> */}
+
     </div>
   )
 }
