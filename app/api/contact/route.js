3a043f7b2e16f156c30fb359bdc8c757efcbf9e1
labidi-mahdi <mahdi.labidi@rstech-solutions.com>
2024-07-23 07:01:48 +0100

    fix route code

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -2,19 +2,29 @@
 import nodemailer from 'nodemailer';
 
 export const POST = async (req) => {
-  const { name, email, message } = await req.json();
-
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
-
   try {
+    const { name, email, message } = await req.json();
+
+    // Check for missing fields
+    if (!name || !email || !message) {
+      return new Response(JSON.stringify({ error: 'All fields are required.' }), {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
     await transporter.sendMail({
       from: process.env.SMTP_USER,
       to: "labidimahdi99@gmail.com",
@@ -29,6 +39,7 @@ export const POST = async (req) => {
       },
     });
   } catch (error) {
+    console.error('Error sending email:', error);
     return new Response(JSON.stringify({ error: 'Failed to send email.' }), {
       status: 500,
       headers: {
