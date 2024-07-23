f8743dddac6959ca07111b199a1a53841a898e4f
labidi-mahdi <mahdi.labidi@rstech-solutions.com>
2024-07-23 06:46:34 +0100

    fix/route

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,4 +1,5 @@
 'use client';
+'use client';
 import React, { useState } from 'react';
 
 const ContactForm = () => {
@@ -13,7 +14,7 @@ const ContactForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('/contact-me', {
+      const response = await fetch('/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
